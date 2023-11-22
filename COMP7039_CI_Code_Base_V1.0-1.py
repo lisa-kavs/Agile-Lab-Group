492aee0a3c0fcc7b139a4a0e38076a5e0ca7f4e1
Dominik Kmita R00220247 <dominik.kmita@mycit.ie>
2023-11-22 11:59:51 +0000

    reverting changes to location data files.

--- COMP7039_CI_Code_Base_V1.0-1.py
+++ COMP7039_CI_Code_Base_V1.0-1.py
@@ -51,7 +51,7 @@ def race_results(races_location):
 
 
 def race_venues():
-    with open("Races.txt") as input:
+    with open("Races-1.txt") as input:
         lines = input.readlines()
     races_location = []
     for line in lines:
